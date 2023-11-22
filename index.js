f1452e1a8ccc5ec572c523128b0007cbd94f3804
alejandriadavila <149810746+alejandriadavila@users.noreply.github.com>
2023-11-22 14:27:50 -0500

    Variable setup

---
+++ index.js
@@ -0,0 +1,19 @@
+// Goals:
+// Monitor plant growth
+// Predict plant growth
+// make decisions based on available space & predicted growth
+
+// Calculate the area of the plant area
+const PI = 3.1415
+const radius = 5
+const area = PI * radius * radius
+
+//Minimum plant space needed for growth
+const plantSpace = .8
+
+// Amount of plants and the projected growth per week
+let starterPlants = 20
+const weekGrowth = starterPlants * 2
+
+// Predict plant growth after a specific number of weeks
+let weeks = 4
