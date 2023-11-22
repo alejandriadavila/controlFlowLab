13caaaae95ae34bbfb388d1d80bda2a574c35055
alejandriadavila <149810746+alejandriadavila@users.noreply.github.com>
2023-11-22 15:58:50 -0500

    Predict Plant Growth & Calculate Capacity Percentage

--- index.js
+++ index.js
@@ -3,17 +3,26 @@
 // Predict plant growth
 // make decisions based on available space & predicted growth
 
-// Calculate the area of the plant area
+// Calculate the area of the plant garden
 const PI = 3.1415
 const radius = 5
 const area = PI * radius * radius
 
 //Minimum plant space needed for growth
 const plantSpace = .8
+const maxCapacity = area / plantSpace
 
 // Amount of plants and the projected growth per week
 let starterPlants = 20
-const weekGrowth = starterPlants * 2
+let weeks = 0
+// let weekGrowth = starterPlants * (2 ** weeks)
+// console.log (weekGrowth)
+    // starterPlants * 2
 
-// Predict plant growth after a specific number of weeks
-let weeks = 4
+    // Predict plant growth after a specific number of weeks
+if (weeks = 1){
+    let totalGrowth = starterPlants * (2 ** weeks)
+    let capacityPercentage = totalGrowth / maxCapacity
+    console.log(capacityPercentage)
+    // Decide whether or not to prune 
+}
