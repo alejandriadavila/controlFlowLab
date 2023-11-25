0421520d467ec151a95032c9c7f3b3fb62fad451
alejandriadavila <149810746+alejandriadavila@users.noreply.github.com>
2023-11-24 20:23:56 -0500

    Part 2 Completed

--- index.js
+++ index.js
@@ -15,14 +15,50 @@ const maxCapacity = area / plantSpace
 // Amount of plants and the projected growth per week
 let starterPlants = 20
 let weeks = 0
-// let weekGrowth = starterPlants * (2 ** weeks)
-// console.log (weekGrowth)
-    // starterPlants * 2
+let totalGrowth = starterPlants * (2 ** weeks)
 
-    // Predict plant growth after a specific number of weeks
+// Predict plant growth after a specific number of weeks
 if (weeks = 1){
     let totalGrowth = starterPlants * (2 ** weeks)
-    let capacityPercentage = totalGrowth / maxCapacity
-    console.log(capacityPercentage)
-    // Decide whether or not to prune 
+    let capacityPercentage = (totalGrowth / maxCapacity) * 100
+        if (capacityPercentage > 80){
+            console.log(`Week 1 Results: The garden is at ${Math.trunc(capacityPercentage)}% capacity, Pruning is recommended.`)
+        } else if(capacityPercentage >= 50){
+            console.log(`Week 1 Results: The garden is at ${Math.trunc(capacityPercentage)}% capacity, Monitoring is recommended.`)
+        } else{
+            console.log(`Week 1 Results: The garden is at ${Math.trunc(capacityPercentage)}% capacity, Additional Planting is recommended.`)
+        }
+    }
+
+if (weeks = 2){
+        let totalGrowth = starterPlants * (2 ** weeks)
+        let capacityPercentage = (totalGrowth / maxCapacity) * 100
+            if (capacityPercentage > 80){
+                console.log(`Week 2 Results: The garden is at ${Math.trunc(capacityPercentage)}% capacity, Pruning is recommended.`)
+            } else if(capacityPercentage >= 50){
+                console.log(`Week 2 Results: The garden is at ${Math.trunc(capacityPercentage)}% capacity, Monitoring is recommended.`)
+            } else{
+                console.log(`Week 2 Results: The garden is at ${Math.trunc(capacityPercentage)}% capacity, Additional Planting is recommended.`)
+            }
+        }
+
+if (weeks = 3){
+            let totalGrowth = starterPlants * (2 ** weeks)
+            let capacityPercentage = (totalGrowth / maxCapacity) * 100
+                if (capacityPercentage > 80){
+                    console.log(`Week 3 Results: The garden is at ${Math.trunc(capacityPercentage)}% capacity, Pruning is recommended.`)
+                } else if(capacityPercentage >= 50){
+                    console.log(`Week 3 Results: The garden is at ${Math.trunc(capacityPercentage)}% capacity, Monitoring is recommended.`)
+                } else{
+                    console.log(`Week 3 Results: The garden is at ${Math.trunc(capacityPercentage)}% capacity, Additional Planting is recommended.`)
+                }
+            }
+
+if (weeks = 10){
+    let starterPlants = 100
+    let totalGrowth = starterPlants * (2 ** weeks)
+    let spaceNeeded = totalGrowth * plantSpace
+    console.log(`The amount of space needed to start with 100 plants and not prune for ten weeks is: ${spaceNeeded} meters`)
+    let newRadius = Math.sqrt(spaceNeeded / PI)
+    console.log(`The reccommended radius for an expanded garden would be ${newRadius}`)
 }
